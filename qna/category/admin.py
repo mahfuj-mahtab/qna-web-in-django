1b229ee4a4af20e92f9a7e194d2b41c961affa41
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-09-23 21:35:52 +0600

    category model created

--- qna/category/admin.py
+++ qna/category/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from category.models import Category
 # Register your models here.
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    pass
+admin.site.register(Category)
--- qna/category/models.py
+++ qna/category/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    id = models.AutoField(primary_key = True)
+    cat_name = models.CharField(max_length=150)
