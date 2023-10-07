3f899711f7a7279e59269b71a0d454ecfc4147bc
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-10-08 00:38:11 +0600

    profile edit done

--- qna/qna/settings.py
+++ qna/qna/settings.py
@@ -38,8 +38,9 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    'home',
+    "home",
     'questions',
+    'answer',
     'category',
 ]
 
