e832912083ecc397e60e9442411e2625d55d0023
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-10-08 00:27:44 +0600

    working on user edit

--- qna/qna/urls.py
+++ qna/qna/urls.py
@@ -17,17 +17,29 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from home.views import *
+from django.conf import settings #add this
+from django.conf.urls.static import static #add this
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", home,name = 'home'),
+    path('admin/', admin.site.urls),
+    path('',home_view,name = "home"),
+    path('about/',about,name = "about"),
+    path('answer/<int:id>/',show_answer_view,name = 'answer'),
+    path('profile/user/<str:u>/', profile_view_other,name="profile view other"),
+    path('profile/user/<str:u>/answer/', profile_view_other_answer,name="profile view other answer"),
+    # path('search/',search_view,name = 'search'),
     path('signup/',signup_view,name = 'signup'),
     path('login/', login, name = "login"),
-    path('logout/', logout, name = 'logout'),
-    path('ask/', ask_view, name = 'ask_view'),
-    path('answer/<int:id>/',show_answer_view,name = 'answer'),
-    path('profile/',profile_view,name = 'profile'),
-    path("verify/",verify,name="verify"),
     path('recover/',recover, name= "recover"),
+    path('ask_question/',ask_question_view,name = 'askquestion'),
+    path('pagination/',pagination_view,name = 'pagination'),
+    path('profile/',profile_view,name = 'profile'),
+    path('profile_setting/',profile_setting_view,name = 'profilesetting'),
     path('category/<str:cat>',category_view,name = 'category'),
+    path('logout/', logout, name = 'logout'),
+    path('profile/answer/', profile_view_answer, name="profile_view"),
+    path('profileedit/', profileedit,name ="Profile edit"),
+    path("upload", upload, name="upload"),
     path("search/", search,name="search"),
-]
+    path("verify/",verify,name="verify"),
+    path('changed/',pass_changed,name = "password changed"),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
