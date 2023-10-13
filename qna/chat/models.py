81145fbcb1f7636903cf1cd73aacbf648d443acb
Mahfuj Mahtab Mohot <mohotmohot1@gmail.com>
2023-10-13 17:30:56 +0600

    msg model working

--- qna/chat/models.py
+++ qna/chat/models.py
@@ -1,1 +1,8 @@
-# 
+from django.db import models
+from home.models import *
+# Create your models here.
+class Message(models.Model):
+    sender = models.ForeignKey(OurUser, on_delete=models.CASCADE, related_name='sent_messages')
+    receiver = models.ForeignKey(OurUser, on_delete=models.CASCADE, related_name='received_messages')
+    content = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
